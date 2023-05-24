33e96774357ca7c5ee2810ebc58dd8b1857a3902
Trudix <91519475+trudix121@users.noreply.github.com>
2023-05-24 22:24:22 +0100

    ada

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -78,6 +78,9 @@ function Home() {
           {tagline}
         </h2>
       </div>
+      <div className="mt-5 md:mt-0">
+        <img ref={myimageref} className="w-1/2 md:ml-auto" src={img} alt="Pavan MG" />
+      </div>
     </main>
   );
 }
